5ff9a4d9c1cc008e82791e025ed82afa88736aeb
geetha-here <geethasaanch@gmail.com>
2025-11-06 21:03:18 +0530

    Update price_gap.py

--- task1/price_gap.py
+++ task1/price_gap.py
@@ -24,3 +24,10 @@ def find_price_gap_pair(nums: list[int], k: int) -> Optional[Tuple[int, int]]:
             earliest[v] = j
 
     return best_pair
+ku=int(input())
+nums = []
+for i in range(ku):
+    yu=int(input())
+    nums.append(yu)
+k = int(input())
+print(find_price_gap_pair(nums,k))
